168f6cc304275753f371ce8758f9794678fb5c23
Lucy Dykhouse <l.dykhouse24@gmail.com>
2021-11-07 15:41:42 -0500

    Finished random fortune request

--- server/index.js
+++ server/index.js
@@ -8,17 +8,33 @@ app.use(cors());
 
 app.use(express.json()); // When we want to be able to accept JSON.
 
+// Request for random compliment
 app.get("/api/compliment", (req, res) => {
   const compliments = ["Gee, you're a smart cookie!",
 					 "Cool shirt!",
 					 "Your Javascript skills are stellar.",
   ];
 
-  // choose random compliment
+  // Choose random compliment
   let randomIndex = Math.floor(Math.random() * compliments.length);
   let randomCompliment = compliments[randomIndex];
 
   res.status(200).send(randomCompliment);
+  
+});
+
+// Request for random fortune
+app.get("/api/fortune", (req, res) => {
+  const fortunes = ["A pleasant surprise is waiting for you.",
+					 "All your hard work will soon pay off.",
+					 "Congratulations! You are on your way.",
+  ];
+
+  // Choose random fortune
+  let randomIndex = Math.floor(Math.random() * fortunes.length);
+  let randomFortune = fortunes[randomIndex];
+
+  res.status(200).send(randomFortune);
   
 });
 
